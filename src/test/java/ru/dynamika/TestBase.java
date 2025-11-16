0eaafc95389d050d1c6f662adf225a1fdb76280d
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-16 17:15:06 +0300

    added MainPage TestBase

---
+++ src/test/java/ru/dynamika/TestBase.java
@@ -0,0 +1,28 @@
+package ru.dynamika;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+
+        Configuration.baseUrl = "https://dynamika.ru";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "127.0");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.timeout = 10000;
+        Configuration.remote = ("https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
+        // Настраиваем capabilities для Selenoid
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+}

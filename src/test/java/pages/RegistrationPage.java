6b9b7f3d536f975aa0a6badf60e109bef2b75e7d
annashelyakina <anna.shelyakina82@gmail.com>
2025-11-17 19:42:27 +0300

    updated RegistrationPage

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -1,7 +1,5 @@
 package pages;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class RegistrationPage {
